14d0546ceb832bc4a4ecef631282a510eacf7f08
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-01-31 11:03:27 -0400

    improve: Define abstract addSession method

--- core/db_base.dart
+++ core/db_base.dart
@@ -1,1 +1,8 @@
-abstract class DB {}
+abstract class DB {
+  void addSession({
+    required String sessionID,
+    required String participantId,
+    required DateTime timeStart,
+    required DateTime timeEnd,
+  }) {}
+}
