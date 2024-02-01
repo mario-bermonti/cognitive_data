352919c5d2018d097472d8fe6f40515e0c10c0d8
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-01 19:43:27 -0400

    test: Define a test for DataManager.addSessionMetadata

--- test/data_manager/data_manager_test.dart
+++ test/data_manager/data_manager_test.dart
@@ -1,5 +1,6 @@
 import 'package:cognitive_data/data_manager/data_manager.dart';
 import 'package:cognitive_data/models/device.dart';
+import 'package:cognitive_data/models/session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,5 +16,29 @@ void main() {
 
     expect(manager.device.participantID, device.participantID);
     expect(manager.device.sessionID, device.sessionID);
+  });
+
+  test(
+      'addSessionMetadata correctly defines a SessionMetadata model and assigns it to the SessionMetadata field',
+      () {
+    final SessionMetadata metadata = SessionMetadata(
+      participantID: '101',
+      sessionID: '001',
+      startTime: DateTime.now(),
+      endTime: DateTime.now(),
+    );
+
+    final DataManager manager = DataManager();
+    manager.addSessionMetadata(
+      sessionID: metadata.sessionID,
+      participantId: metadata.participantID,
+      timeStart: metadata.startTime,
+      timeEnd: metadata.endTime,
+    );
+
+    expect(manager.sessionMetadata.participantID, metadata.participantID);
+    expect(manager.sessionMetadata.sessionID, metadata.sessionID);
+    expect(manager.sessionMetadata.startTime, metadata.startTime);
+    expect(manager.sessionMetadata.endTime, metadata.endTime);
   });
 }
