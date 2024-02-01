aa0b8177cf1c65db54465a874dfba36a33b4eb23
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-01 18:32:46 -0400

    refactor: Clean test for Session.toString()

--- test/models/session_test.dart
+++ test/models/session_test.dart
@@ -3,16 +3,17 @@ import 'package:test/test.dart';
 
 void main() {
   test('SessionMetadata.toString produces correct representation', () {
-    final DateTime startTime = DateTime.now();
-    final DateTime endTime = DateTime.now();
-    final String strRep = "Session(participantID: 101, sessionID: 001, "
-        "startTime: $startTime, endTime: $endTime)";
     final SessionMetadata sessionMetadata = SessionMetadata(
       participantID: '101',
       sessionID: '001',
-      startTime: startTime,
-      endTime: endTime,
+      startTime: DateTime.now(),
+      endTime: DateTime.now(),
     );
+    final String strRep =
+        "Session(participantID: ${sessionMetadata.participantID}, "
+        "sessionID: ${sessionMetadata.sessionID}, "
+        "startTime: ${sessionMetadata.startTime}, "
+        "endTime: ${sessionMetadata.endTime})";
     expect(sessionMetadata.toString(), strRep);
   });
 }
