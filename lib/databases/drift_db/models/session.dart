e3aea1124d003bca310a5b32ee04e48aa000eef6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-07 10:07:49 -0400

    improve: Impl drift session constructor

--- lib/databases/drift_db/models/session.dart
+++ lib/databases/drift_db/models/session.dart
@@ -1,9 +1,23 @@
+import 'package:cognitive_data/databases/drift_db/drift_db.dart';
+import 'package:cognitive_data/models/session.dart';
 import 'package:drift/drift.dart';
 
-class Sessions extends Table {
+class SessionMetadata extends Table {
   IntColumn get id => integer().autoIncrement()();
-  TextColumn get sessionID => text()();
   TextColumn get participantID => text()();
+  TextColumn get sessionID => text()();
   DateTimeColumn get startTime => dateTime()();
   DateTimeColumn get endTime => dateTime()();
+
+  static SessionMetadataCompanion fromSessionMetadata(
+    BaseSessionMetadata baseSessionsMetadata,
+  ) {
+    final SessionMetadataCompanion sessionMetadata = SessionMetadataCompanion(
+      participantID: Value(baseSessionsMetadata.participantID),
+      sessionID: Value(baseSessionsMetadata.sessionID),
+      startTime: Value(baseSessionsMetadata.startTime),
+      endTime: Value(baseSessionsMetadata.endTime),
+    );
+    return sessionMetadata;
+  }
 }
