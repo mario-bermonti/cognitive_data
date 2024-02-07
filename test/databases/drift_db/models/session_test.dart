3196d14642a4633048a7500d1ab8cce412c1877c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-07 10:06:59 -0400

    test: Define test for drift session constructor

--- test/databases/drift_db/models/session_test.dart
+++ test/databases/drift_db/models/session_test.dart
@@ -14,4 +14,22 @@ void main() {
   tearDown(() async {
     await db.close();
   });
+
+  test('Correctly instantiate a Drift.Session object from a base session model',
+      () async {
+    final BaseSessionMetadata baseSession = BaseSessionMetadata(
+      participantID: '101',
+      sessionID: '001',
+      startTime: DateTime.now(),
+      endTime: DateTime.now(),
+    );
+
+    final SessionMetadataCompanion session =
+        await SessionMetadata.fromSessionMetadata(baseSession);
+
+    expect(session.participantID.value, baseSession.participantID);
+    expect(session.sessionID.value, baseSession.sessionID);
+    expect(session.startTime.value, baseSession.startTime);
+    expect(session.endTime.value, baseSession.endTime);
+  });
 }
