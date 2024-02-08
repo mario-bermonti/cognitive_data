391077c754f0473d5a5bc78779dff7dab759c658
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-08 10:06:45 -0400

    test: Define a test for DriftDevice.fromDevice
    test: Init bindings when testing DriftDevice
    
    test: Make DriftDevice.fromDevice test sync
    
    test: Fix test by comparing value properties
    
    maint: Remove comments

--- test/databases/drift_db/models/device_test.dart
+++ test/databases/drift_db/models/device_test.dart
@@ -1,4 +1,6 @@
 import 'package:cognitive_data/databases/drift_db/drift_db.dart';
+import 'package:cognitive_data/databases/drift_db/models/device.dart';
+import 'package:cognitive_data/models/device.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,4 +14,26 @@ void main() {
   tearDown(() async {
     await db.close();
   });
+
+  test(
+    "Correctly instantiate a DriftDevice object from a base Device model",
+    () {
+      TestWidgetsFlutterBinding.ensureInitialized();
+
+      final Device baseDevice = Device(
+        participantID: '101',
+        sessionID: '001',
+      );
+
+      final DriftDeviceCompanion driftDevice =
+          DriftDevice.fromDevice(baseDevice);
+
+      expect(driftDevice.participantID.value, baseDevice.participantID);
+      expect(driftDevice.sessionID.value, baseDevice.sessionID);
+      expect(driftDevice.platform.value, baseDevice.platform);
+      expect(driftDevice.height.value, baseDevice.height);
+      expect(driftDevice.width.value, baseDevice.width);
+      expect(driftDevice.aspectRatio.value, baseDevice.aspectRatio);
+    },
+  );
 }
