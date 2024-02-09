d4d745f64b1bac0297d1779fceef792345a53c83
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-08 21:36:09 -0400

    improve: Implement DriftDevice.fromDevice

--- lib/databases/drift_db/models/device.dart
+++ lib/databases/drift_db/models/device.dart
@@ -1,5 +1,8 @@
+import 'package:cognitive_data/databases/drift_db/drift_db.dart';
 import 'package:cognitive_data/databases/drift_db/models/session.dart';
 import 'package:drift/drift.dart';
+
+import '../../../models/device.dart';
 
 class DriftDevice extends Table {
   IntColumn get id => integer().autoIncrement()();
@@ -11,4 +14,15 @@ class DriftDevice extends Table {
   RealColumn get height => real()();
   RealColumn get width => real()();
   RealColumn get aspectRatio => real()();
+
+  static DriftDeviceCompanion fromDevice(Device baseDevice) {
+    final DriftDeviceCompanion driftDevice = DriftDeviceCompanion(
+        participantID: Value(baseDevice.participantID),
+        sessionID: Value(baseDevice.sessionID),
+        platform: Value(baseDevice.platform),
+        height: Value(baseDevice.height),
+        width: Value(baseDevice.width),
+        aspectRatio: Value(baseDevice.aspectRatio));
+    return driftDevice;
+  }
 }
