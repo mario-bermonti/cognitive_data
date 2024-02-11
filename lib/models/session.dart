777baec79c1b3c03dc34367bd0b42d47762a2e60
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-11 10:24:27 -0400

    doc: Add docstrings to base DriftSessionMetadata

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,8 +1,15 @@
 /// Represents the metadata for data collection session.
 class SessionMetadata {
+  /// Unique identifier for the participant
   final String participantID;
+
+  /// Unique identifier for the current session of the  current participant.
   final String sessionID;
+
+  /// Time at which the session started.
   final DateTime startTime;
+
+  /// Time at which the session ended.
   final DateTime endTime;
 
   SessionMetadata({
