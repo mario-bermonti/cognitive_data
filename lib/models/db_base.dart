ed01d40148b2717678801bc71a2a9ec831edbbce
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-12 09:23:59 -0400

    refactor: Require base Session model in DB ABC

--- lib/models/db_base.dart
+++ lib/models/db_base.dart
@@ -1,14 +1,10 @@
+import 'package:cognitive_data/models/session.dart';
 import 'trial_type.dart';
 
 /// Serves as the design contract between the different implementations of
 /// the databases.
 abstract class DB {
-  void addSessionMetadata({
-    required String sessionID,
-    required String participantID,
-    required DateTime startTime,
-    required DateTime endTime,
-  }) {}
+  void addSessionMetadata({required SessionMetadata session}) {}
 
   void addDevice({
     required String sessionID,
