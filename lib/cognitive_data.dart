98d88fcd9e7624b54bb056f14be078b04f00d2d6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-12 19:44:26 -0400

    improve: Expose the base models to users

--- lib/cognitive_data.dart
+++ lib/cognitive_data.dart
@@ -1,4 +1,7 @@
 library cognitive_data;
 
-export 'package:cognitive_data/db.dart';
+// export 'package:cognitive_data/db.dart';
 export 'package:cognitive_data/models/trial_type.dart';
+export 'package:cognitive_data/models/trial.dart';
+export 'package:cognitive_data/models/device.dart';
+export 'package:cognitive_data/models/session.dart';
