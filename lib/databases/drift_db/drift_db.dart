e5c48978a0e100204bfc60a3d7f9707625f108e7
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-13 08:48:07 -0400

    improve: Implement DB in DriftDB

--- lib/databases/drift_db/drift_db.dart
+++ lib/databases/drift_db/drift_db.dart
@@ -1,14 +1,34 @@
 import 'package:cognitive_data/databases/drift_db/models/device.dart';
 import 'package:cognitive_data/databases/drift_db/models/session.dart';
 import 'package:cognitive_data/databases/drift_db/models/trial.dart';
+import 'package:cognitive_data/models/trial.dart';
+import 'package:cognitive_data/models/session.dart';
+import 'package:cognitive_data/models/device.dart';
 import 'package:drift/drift.dart';
+
+import '../../models/db_base.dart';
 
 part 'drift_db.g.dart';
 
 @DriftDatabase(tables: [DriftSessionMetadata, DriftDevice, DriftTrial])
-class DriftDB extends _$DriftDB {
+class DriftDB extends _$DriftDB implements DB {
   DriftDB(QueryExecutor dbConnection) : super(dbConnection);
 
   @override
   int get schemaVersion => 1;
+
+  @override
+  void addDevice({required Device device}) {
+    // TODO: implement addDevice
+  }
+
+  @override
+  void addSessionMetadata({required SessionMetadata session}) {
+    // TODO: implement addSessionMetadata
+  }
+
+  @override
+  void addTrial({required Trial trial}) {
+    // TODO: implement addTrial
+  }
 }
