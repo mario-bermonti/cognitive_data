9a60cd41652157f12a21ef34671f5f22b89a94e3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-13 09:07:44 -0400

    test: Add setup teardown funs DriftDB tests

---
+++ test/databases/drift_db/drift_db_test.dart
@@ -0,0 +1,15 @@
+import 'package:cognitive_data/databases/drift_db/drift_db.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late final DriftDB db;
+
+  setUp(() {
+    db = DriftDB(NativeDatabase.memory());
+  });
+
+  tearDown(() async {
+    await db.close();
+  });
+u}
