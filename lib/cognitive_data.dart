63c4277f04939ea274447edbaf20543e1dc90876
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-18 12:26:03 -0400

    improve: Export the DriftDB

--- lib/cognitive_data.dart
+++ lib/cognitive_data.dart
@@ -2,6 +2,7 @@ library cognitive_data;
 
 // export 'package:cognitive_data/db.dart';
 export 'package:cognitive_data/databases/in_memory_db.dart';
+export 'package:cognitive_data/databases/drift_db/drift_db.dart';
 export 'package:cognitive_data/models/trial_type.dart';
 export 'package:cognitive_data/models/trial.dart';
 export 'package:cognitive_data/models/device.dart';
