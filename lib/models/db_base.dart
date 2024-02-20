5146f6e2d8625ba9b4b041d10860fd658de3a4d0
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-20 08:42:50 -0400

    maint: Remove unused import

--- lib/models/db_base.dart
+++ lib/models/db_base.dart
@@ -2,8 +2,6 @@ import 'package:cognitive_data/models/device.dart';
 import 'package:cognitive_data/models/session.dart';
 import 'package:cognitive_data/models/trial.dart';
 
-import 'trial_type.dart';
-
 /// Serves as the design contract between the different implementations of
 /// the databases.
 abstract class DB {
