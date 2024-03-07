f08634a5f4ffd13b007cd6d532829abf4e83baa3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-07 15:43:20 -0400

    improve: Declare the FirebaseDB

---
+++ lib/databases/firebase_db/firebase_db.dart
@@ -0,0 +1,27 @@
+import 'package:cognitive_data/models/db_base.dart';
+import 'package:cognitive_data/models/trial.dart';
+import 'package:cognitive_data/models/session.dart';
+import 'package:cognitive_data/models/device.dart';
+
+/// DB used to store data in Firebase.
+class FirebaseDB implements DB {
+  @override
+  void addDevice({required Device device}) {
+    // TODO: implement addDevice
+  }
+
+  @override
+  void addSession({required Session session}) {
+    // TODO: implement addSession
+  }
+
+  @override
+  void addTrial({required Trial trial}) {
+    // TODO: implement addTrial
+  }
+
+  @override
+  void addTrials({required List<Trial> trials}) {
+    // TODO: implement addTrials
+  }
+}
