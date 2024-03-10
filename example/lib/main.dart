aa19cec313d738b4a6be7cef00341b20ab1b99ab
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-10 08:51:33 -0400

    maint(example): Remove code related to counter app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,13 +29,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // String _message = '';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // void _updateMessage(String message) {
+  //   setState(() {
+  //     _message = message;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -47,20 +47,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Save data to firebase',
+              style: Theme.of(context).textTheme.headline6,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
