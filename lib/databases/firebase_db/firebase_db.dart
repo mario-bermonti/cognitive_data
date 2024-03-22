758b900a7c8295a25a8428af4ad01d6de3adb198
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-22 10:45:17 -0400

    doc: Add docstring to FirebaseDB.addSession

--- lib/databases/firebase_db/firebase_db.dart
+++ lib/databases/firebase_db/firebase_db.dart
@@ -45,6 +45,12 @@ class FirebaseDB implements DB {
     // TODO: implement addDevice
   }
 
+  /// Adds metadata for the [session] to [FirebaseFirestore].
+  ///
+  /// It will override any data previously saved for the current session. The
+  /// current session is determined based on the [participantID], [sessionID],
+  /// and [taskName] specified when the [FirebaseDB] was instantiated, and not
+  /// from the values in [session].
   @override
   Future<void> addSession({required Session session}) async {
     final CollectionReference dataRef = _db.collection(
