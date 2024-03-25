3314fdba8c6566deaac54bbda7d896509150505d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-03-25 08:46:52 -0400

    refactor: Improve code org FirebaseDB.addSession

--- lib/databases/firebase_db/firebase_db.dart
+++ lib/databases/firebase_db/firebase_db.dart
@@ -74,15 +74,16 @@ class FirebaseDB implements DB {
   /// from the values in [session].
   @override
   Future<void> addSession({required Session session}) async {
-    final CollectionReference dataRef = _db.collection(
-        'participants/$participantID/cognitive_tasks/$taskName/sessions');
-
     final Map<String, dynamic> sessionData = {
       'participantID': session.participantID,
       'sessionID': session.sessionID,
       'startTime': session.startTime.toString(),
       'endTime': session.endTime.toString(),
     };
+
+    final CollectionReference dataRef = _db.collection(
+        'participants/$participantID/cognitive_tasks/$taskName/sessions');
+
     await dataRef.doc(sessionID).set(
       {'sessionMetadata': sessionData},
       SetOptions(merge: true),
