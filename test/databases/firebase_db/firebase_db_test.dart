f71cd4ec4854d39c619e0c4feec71b1d7d3196f9
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-04-02 10:38:49 -0400

    test(firebase): Add test for FirebaseDB.addSession

--- test/databases/firebase_db/firebase_db_test.dart
+++ test/databases/firebase_db/firebase_db_test.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cognitive_data/databases/firebase_db/firebase_db.dart';
+import 'package:cognitive_data/models/session.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -16,4 +18,27 @@ void main() {
     currentSessionPath =
         'participants/${db.participantID}/cognitive_tasks/${db.taskName}/sessions/${db.sessionID}';
   });
+
+  test(
+    "FirebaseDB.addSession correctly adds data to Firebase",
+    () async {
+      final Session sessionLocal = Session(
+        participantID: db.participantID,
+        sessionID: db.sessionID,
+        startTime: DateTime.now(),
+        endTime: DateTime.now(),
+      );
+
+      await db.addSession(session: sessionLocal);
+
+      final DocumentSnapshot sessionFirebaseSnapshot = await db.db
+          .doc('$currentSessionPath/sessionMetadata/sessionMetadata')
+          .get();
+
+      final Map<String, dynamic> sessionFirebaseData =
+          sessionFirebaseSnapshot.data() as Map<String, dynamic>;
+
+      expect(sessionFirebaseData, sessionLocal.toJson());
+    },
+  );
 }
